c509eeb50d49a10a32b3c3f82a849eb30df3a456
Git-PedroNeri <neri.pedro@icloud.com>
2025-04-15 20:34:03 -0300

    update

---
+++ accounts/src/test/java/com/nexopay/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexopay.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
