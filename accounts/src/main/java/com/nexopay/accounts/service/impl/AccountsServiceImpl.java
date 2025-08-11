0c6fee7a9d1931410727ea3cbf86864b5d92d0b5
Git-PedroNeri <neri.pedro@icloud.com>
2025-08-11 09:43:28 -0300

    update

--- accounts/src/main/java/com/nexopay/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/nexopay/accounts/service/impl/AccountsServiceImpl.java
@@ -2,6 +2,7 @@ package com.nexopay.accounts.service.impl;
 
 import com.nexopay.accounts.constants.AccountsConstants;
 import com.nexopay.accounts.dto.AccountsDto;
+import com.nexopay.accounts.dto.AccountsMsgDto;
 import com.nexopay.accounts.dto.CustomerDto;
 import com.nexopay.accounts.entity.Accounts;
 import com.nexopay.accounts.entity.Customer;
@@ -13,6 +14,9 @@ import com.nexopay.accounts.repository.AccountsRepository;
 import com.nexopay.accounts.repository.CustomerRepository;
 import com.nexopay.accounts.service.IAccountsService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -22,8 +26,10 @@ import java.util.Random;
 @AllArgsConstructor
 public class AccountsServiceImpl  implements IAccountsService {
 
+    private static final Logger log = LoggerFactory.getLogger(AccountsServiceImpl.class);
     private AccountsRepository accountsRepository;
     private CustomerRepository customerRepository;
+    private final StreamBridge streamBridge;
 
     /**
      * @param customerDto - CustomerDto Object
@@ -37,7 +43,16 @@ public class AccountsServiceImpl  implements IAccountsService {
                     +customerDto.getMobileNumber());
         }
         Customer savedCustomer = customerRepository.save(customer);
-        accountsRepository.save(createNewAccount(savedCustomer));
+        Accounts savedAccount = accountsRepository.save(createNewAccount(savedCustomer));
+        sendCommunication(savedAccount, savedCustomer);
+    }
+
+    private void sendCommunication(Accounts account, Customer customer) {
+        var accountsMsgDto = new AccountsMsgDto(account.getAccountNumber(), customer.getName(),
+                customer.getEmail(), customer.getMobileNumber());
+        log.info("Sending Communication request for the details: {}", accountsMsgDto);
+        var result = streamBridge.send("sendCommunication-out-0", accountsMsgDto);
+        log.info("Is the Communication request successfully triggered ? : {}", result);
     }
 
     /**
--- gatewayserver/src/main/java/com/nexopay/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/nexopay/gatewayserver/GatewayserverApplication.java
@@ -78,5 +78,4 @@ public class GatewayserverApplication {
                         .defaultIfEmpty("anonymous");
     }
 
-
 }
